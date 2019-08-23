13e5cd0655d30129c71b2993ffb4c3d32a3a661d
ahmed <ahmed.abdelkarim@nozol.com>
2019-08-23 22:22:47 +0200

    Updates

---
+++ precache-manifest.19d3c235f2dc4a0014ac2c91d16a6679.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33efb02bb1cd767f0f77da86707eb2fd",
+    "url": "/nasnav/index.html"
+  },
+  {
+    "revision": "dc7fa1d8e95c2a88973e",
+    "url": "/nasnav/static/css/main.a5376aac.chunk.css"
+  },
+  {
+    "revision": "929320b1c25bfd7ffe16",
+    "url": "/nasnav/static/js/2.c113873c.chunk.js"
+  },
+  {
+    "revision": "dc7fa1d8e95c2a88973e",
+    "url": "/nasnav/static/js/main.4b6ea4be.chunk.js"
+  },
+  {
+    "revision": "395076fe3b1704c1ce16",
+    "url": "/nasnav/static/js/runtime~main.85561905.js"
+  },
+  {
+    "revision": "fd4bcb3eda6e93c3c8c6502bca05ac12",
+    "url": "/nasnav/static/media/branch.fd4bcb3e.jpg"
+  },
+  {
+    "revision": "07f36ae447dd0111b80eb13d81dde535",
+    "url": "/nasnav/static/media/c1.07f36ae4.jpg"
+  },
+  {
+    "revision": "0ee0b8b6abc3fe7e9ca2c4c3155494ed",
+    "url": "/nasnav/static/media/c2.0ee0b8b6.jpg"
+  },
+  {
+    "revision": "67cb4b05bbe7812b83b5d8740ee9e12c",
+    "url": "/nasnav/static/media/c3.67cb4b05.jpg"
+  },
+  {
+    "revision": "7d874b5af035f2748d27a5ab638a3c23",
+    "url": "/nasnav/static/media/c4.7d874b5a.jpg"
+  },
+  {
+    "revision": "fd91dc6bf6dc9acab1f0521e7d74f300",
+    "url": "/nasnav/static/media/credit.fd91dc6b.png"
+  },
+  {
+    "revision": "db4c87028a0bc51dc1af98bba7745cee",
+    "url": "/nasnav/static/media/master.db4c8702.png"
+  },
+  {
+    "revision": "8967659b9caf49f9b35687e913c3e469",
+    "url": "/nasnav/static/media/p1.8967659b.png"
+  },
+  {
+    "revision": "36b5fd505516df322f9157d2fb132ae3",
+    "url": "/nasnav/static/media/p2.36b5fd50.png"
+  },
+  {
+    "revision": "32d576ce5cb116b7f139482f96e812c2",
+    "url": "/nasnav/static/media/p3.32d576ce.png"
+  },
+  {
+    "revision": "99c4634b7bd2f9c889e409c12479e631",
+    "url": "/nasnav/static/media/p4.99c4634b.png"
+  },
+  {
+    "revision": "6581ad2ffe8188834ea3527ce1f3bb6c",
+    "url": "/nasnav/static/media/smart.6581ad2f.jpg"
+  },
+  {
+    "revision": "13ee79b0b3365d4f8b43f02183a4beaa",
+    "url": "/nasnav/static/media/sweets.13ee79b0.png"
+  },
+  {
+    "revision": "f51f805a6b5bcf9b6eabe6c3e08ddb10",
+    "url": "/nasnav/static/media/tv.f51f805a.png"
+  },
+  {
+    "revision": "d385d7ffb327067893883431f2522ba9",
+    "url": "/nasnav/static/media/visa.d385d7ff.png"
+  },
+  {
+    "revision": "79d6120df2d8191648d1dbf88afbbaf7",
+    "url": "/nasnav/static/media/water.79d6120d.png"
+  }
+]);
